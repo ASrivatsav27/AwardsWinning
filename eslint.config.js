dbed8fe57d3f4bc630f19fc41d17bbc63f625586
Adapa Srivatsav <srivatsava47@gmail.com>
2025-12-07 14:04:22 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
@@ -22,7 +23,7 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
-    rules: {
+    rules: {//
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
